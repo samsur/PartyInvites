171ea7068991ec37aba1552a6d1e6d6bcad67106
Sam <samdup.surkhang@gmail.com>
2025-09-22 18:54:59 -0400

    update the controller action and related view and model to show the form and the list of guests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,26 +6,27 @@ namespace PartyInvites.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+     [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+ 
+       [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+           if (ModelState.IsValid) {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else {
+                return View();
+            }
+        }
+         public ViewResult ListResponses() {
+            return View(Repository.Responses
+                .Where(r => r.WillAttend == true));
+        }
 }
